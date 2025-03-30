6ab248bc60ea9d4e014fc07b688d66f295cd5101
rnadomingos <rna.domingos@hotmail.com>
2025-03-30 10:30:48 -0300

    refactor: adicionar a constante bonus removendo hard code e identando o codigo

--- aula01/kpi.py
+++ aula01/kpi.py
@@ -4,10 +4,19 @@
 # 4. O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
 # 5. Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
 
+CONST_BONUS = 1000
+
+# 1. O programa deve começar solicitando ao usuário que insira seu nome.
 nome = input("Informe seu nome: ")
+
+# 2. Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. 
 vlr_salario = float(input("Qual é o valor de seu salário? "))
+
+# 3. Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário
 perc_bonus = float(input("Qual é o percentual de bonus recebido? "))
 
-bonus = 1000 + vlr_salario * perc_bonus
+# 4. O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
+bonus = CONST_BONUS + vlr_salario * perc_bonus
 
-print("Olá "+ str(nome) + ", o seu valor bônus foi de "+ str(bonus))
+# 5. O programa deve imprimir uma mensagem
+print("Olá "+ str(nome) + ", o seu valor bônus foi de "+ str(bonus)) 
