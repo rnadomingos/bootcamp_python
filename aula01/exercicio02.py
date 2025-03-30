b192b86cb43ab87850b46d9d52f72b3b0d0c3437
rnadomingos <rna.domingos@hotmail.com>
2025-03-30 09:45:01 -0300

    exercicio 03 - refatorando exercicio 02 e atribuindo variaveis

--- aula01/exercicio02.py
+++ aula01/exercicio02.py
@@ -1,5 +1,3 @@
 # Criar um programa onde o usuário digite dois valores e apareça a soma
 
-
-
 print(int(input("Digite um valor: ")) + int(input("Digite outro valor: ")))
---
+++ aula01/exercicio03.py
@@ -0,0 +1,5 @@
+# Refatore o exercício 02 atribuindo variáveis
+
+v1 = int(input("Digite um valor: "))
+v2 = int(input("Digite outro valor: "))
+print(v1 + v2)
