7d4d5be6ece9c472bac0485deb27b00603deb694
rnadomingos <rna.domingos@hotmail.com>
2025-04-02 21:35:06 -0300

    Adicionar multiplicacao de 2 valores

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -9,13 +9,21 @@
 
 #   - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
 
-DIVISOR = 5
-valor1 = int(input("Informe um número: "))
+# DIVISOR = 5
+# valor1 = int(input("Informe um número: "))
 
-resultado = valor1 % DIVISOR
+# resultado = valor1 % DIVISOR
 
-print(f"O resto da divisao é {resultado}.")
+# print(f"O resto da divisao é {resultado}.")
+
 #   - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
+
+valor1 = int(input("Informe um número: "))
+valor2 = int(input("Informe outro número: "))
+
+multiplicacao_valor = valor1 * valor2
+print(f"Os valores multiplicados é igual a {multiplicacao_valor}.")
+
 #   - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #   - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
 # ## Números de Ponto Flutuante (float)
