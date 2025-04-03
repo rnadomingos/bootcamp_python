62aa110d4675f36bd6de854f3026d3e542acd55b
rnadomingos <rna.domingos@hotmail.com>
2025-04-02 21:33:26 -0300

    Adicionar o resto da divisao por 5

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -8,6 +8,13 @@
 # print(f"A soma dos valores é igual a {soma_valor}.")
 
 #   - Crie um programa que receba um número do usuário e calcule o resto da divisão desse número por 5.
+
+DIVISOR = 5
+valor1 = int(input("Informe um número: "))
+
+resultado = valor1 % DIVISOR
+
+print(f"O resto da divisao é {resultado}.")
 #   - Desenvolva um programa que multiplique dois números fornecidos pelo usuário e mostre o resultado.
 #   - Faça um programa que peça dois números inteiros e imprima a divisão inteira do primeiro pelo segundo.
 #   - Escreva um programa que calcule o quadrado de um número fornecido pelo usuário.
