5d0939f066ce9dddb8baac9fe4b026c7ee06abaa
rnadomingos <rna.domingos@hotmail.com>
2025-04-05 08:37:40 -0300

    Adicionar entrada do usuario em minuscula

--- aula02/exercicios.py
+++ aula02/exercicios.py
@@ -96,14 +96,21 @@ import math
 
 # ## Strings (str)
 #   - Escreva um programa que receba uma string do usuário e a converta para maiúsculas.
-print("### Upper Case ### \n")
+# print("### Upper Case ### \n")
 
-string = input("Informe o texto: ")
-upper = string.upper()
+# string = input("Informe o texto: ")
+# upper = string.upper()
 
-print(f"{upper}")
+# print(f"{upper}")
 
 #   - Crie um programa que receba o nome completo do usuário e imprima o nome com todas as letras minúsculas.
+
+print("### Lower Case ### \n")
+
+string = input("Informe o nome completo: ")
+low = string.lower()
+
+print(low)
 #   - Desenvolva um programa que peça ao usuário para inserir uma frase e, em seguida, imprima esta frase sem espaços em branco no início e no final.
 #   - Faça um programa que peça ao usuário para digitar uma data no formato "dd/mm/aaaa" e, em seguida, imprima o dia, o mês e o ano separadamente.
 #   - Escreva um programa que concatene duas strings fornecidas pelo usuário.
