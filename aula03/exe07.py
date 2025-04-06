6cc397820f85150ed9a63d3479d15f36fd67774f
rnadomingos <rna.domingos@hotmail.com>
2025-04-06 14:05:34 -0300

    Adicionar Normalizacao de uma lista para ficar na escala de 0 a 1

---
+++ aula03/exe07.py
@@ -0,0 +1,9 @@
+# Objetivo: Normalizar uma lista de números para que fiquem na escala de 0 a 1.
+
+
+numeros = [10, 20, 30, 40, 50]
+minimo = min(numeros)
+maximo = max(numeros)
+normalizados = [(x - minimo) / (maximo - minimo) for x in numeros]
+
+print(normalizados)
