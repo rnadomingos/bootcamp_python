9cd96a8b25f0b38047de74a050aa045e41747f8f
rnadomingos <rna.domingos@hotmail.com>
2025-04-13 10:41:31 -0300

    Adicionar a remocao de item em lista

---
+++ aula04/exe02.py
@@ -0,0 +1,21 @@
+# 2) Dada a lista ["Python", "Java", "C++", "JavaScript"], remova o item "C++" e adicione "Ruby".
+
+lista = ["Python", "Java", "C++", "JavaScript"]
+
+print("lista completa: ", lista)
+
+#usando remove do lists
+lista.remove("C++")
+print("lista sem o C++: usando o remove", lista)
+
+
+# usando o pop em um for
+lista2 = ["Python", "Java", "C++", "JavaScript"]
+
+print("lista completa: ", lista2)
+
+for l in lista2:
+  if l == 'C++':
+    lista2.remove(l) 
+
+print("lista sem o C++: usando o um for", lista2)
