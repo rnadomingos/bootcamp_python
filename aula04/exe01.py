566803a182dd176ae6c37c167a8a8f6fa7ad8fef
rnadomingos <rna.domingos@hotmail.com>
2025-04-13 08:58:10 -0300

    Adicionar uma lista e calcular o numero elevado ao quadrado

--- aula04/exe01.py
+++ aula04/exe01.py
@@ -1,7 +1,9 @@
+# 1) Crie uma lista com os números de 1 a 10 e use um loop para imprimir cada número elevado ao quadrado.
 
-lista = list(range(1,11))
+lista = list(range(1,12))
 
-for num in lista:
-  print(f"este é a lista de números elevado a 2: {num} {lista[num] ** 2}" )
-
-print(lista)
+for i in lista:
+  print(f"este é a lista de números elevado a 2: indice: {i} e o resultado é: {i ** 2}" )
+  
+  
+print("Lista Geral: ",lista)
